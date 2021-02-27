d0f1300bc637449f3a270e07e89acdfbaabe434d
rparundekar <rahul@elevate.do>
2021-02-26 16:17:54 -0800

    black format

--- app/app.py
+++ app/app.py
@@ -5,32 +5,36 @@ from flask_cors import CORS
 import belief_helper
 
 # The flask api for serving endpoints
-app = Flask(__name__, static_url_path='', 
-            static_folder='/app/static')
+app = Flask(__name__, static_url_path="", static_folder="/app/static")
 CORS(app)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def index():
     return redirect("/index.html")
 
-@app.route('/health_check', methods=['GET'])
+
+@app.route("/health_check", methods=["GET"])
 def ping():
     """ Healthcheck """
     return "ok", 200
 
-@app.route('/beliefs', methods=['POST'])
+
+@app.route("/beliefs", methods=["POST"])
 def remember_belief():
     belief_text = request.get_data().decode("utf-8")
     err, belief_id, entity_id, signature, claims = belief_helper.validate(belief_text)
-    if err: return err, 422
+    if err:
+        return err, 422
     err = belief_helper.remember_belief(belief_id, belief_text)
-    if err: return err, 422
+    if err:
+        return err, 422
     return belief_text, 200
 
-@app.route('/beliefs/<belief_id>', methods=['GET'])
-def get_belief(belief_id:str):
+
+@app.route("/beliefs/<belief_id>", methods=["GET"])
+def get_belief(belief_id: str):
     err, presigned_url = belief_helper.get_belief(belief_id)
-    if err: return err, 422
+    if err:
+        return err, 422
     return redirect(presigned_url)
-
-
--- app/belief_helper.py
+++ app/belief_helper.py
@@ -7,18 +7,28 @@ from minio import Minio
 from minio.error import S3Error
 from datetime import timedelta
 
-minio_client = Minio(os.getenv('S3_URL'),
-                access_key=os.getenv('S3_ACCESS_KEY'),
-                secret_key=os.getenv('S3_SECRET_KEY'),
-                region=os.getenv('S3_REGION_NAME'),
-                secure=(os.getenv('S3_SECURE', "false")=="true"))
+minio_client = Minio(
+    os.getenv("S3_URL"),
+    access_key=os.getenv("S3_ACCESS_KEY"),
+    secret_key=os.getenv("S3_SECRET_KEY"),
+    region=os.getenv("S3_REGION_NAME"),
+    secure=(os.getenv("S3_SECURE", "false") == "true"),
+)
 
-BELIEF_BUCKET = os.getenv('S3_BUCKET_NAME')
+BELIEF_BUCKET = os.getenv("S3_BUCKET_NAME")
+
 
 def validate(belief_text):
     lines = belief_text.splitlines()
-    if len(lines) < 4: return f"Belief document seems incomplete. It needs to have the belief identifier, at least one claim, believer's identifier, and believer's signature on new lines - currently the document only has {len(lines)} lines.", None, None, None, None
-    
+    if len(lines) < 4:
+        return (
+            f"Belief document seems incomplete. It needs to have the belief identifier, at least one claim, believer's identifier, and believer's signature on new lines - currently the document only has {len(lines)} lines.",
+            None,
+            None,
+            None,
+            None,
+        )
+
     belief_id = lines[0]
     # TODO: validate belief id
 
@@ -28,11 +38,12 @@ def validate(belief_text):
     signature = lines[-1]
     # TODO: validate signature
 
-    claims = lines[1:len(lines)-2]
+    claims = lines[1 : len(lines) - 2]
     # TODO: validate claims
 
     return None, belief_id, entity_id, signature, claims
-    
+
+
 def remember_belief(belief_id, belief_text):
     # get s3 location of the belief
     m = hashlib.md5()
@@ -40,7 +51,7 @@ def remember_belief(belief_id, belief_text):
     belief_id_hash = m.hexdigest()
     key = f"{belief_id_hash}.belief"
     try:
-        with tempfile.NamedTemporaryFile(suffix='.belief') as ntf:
+        with tempfile.NamedTemporaryFile(suffix=".belief") as ntf:
             with open(ntf.name, "w") as f:
                 f.write(belief_text)
             minio_client.fput_object(BELIEF_BUCKET, key, ntf.name)
@@ -50,6 +61,7 @@ def remember_belief(belief_id, belief_text):
 
     return None
 
+
 def get_belief(belief_id):
     # get s3 location of the belief
     m = hashlib.md5()
@@ -57,11 +69,10 @@ def get_belief(belief_id):
     belief_id_hash = m.hexdigest()
     key = f"{belief_id_hash}.belief"
     try:
-        presigned_url = minio_client.presigned_get_object(BELIEF_BUCKET, key, expires=timedelta(hours=2))
+        presigned_url = minio_client.presigned_get_object(
+            BELIEF_BUCKET, key, expires=timedelta(hours=2)
+        )
         return None, presigned_url
     except Exception as e:
         traceback.print_exc()
         return f"{e}", None
-
-
-    
